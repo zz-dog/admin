d7b8ece1101ac5c96c3942e93ba3d0b3f79c6537
zz <2286807814@qq.com>
2026-08-21 15:25:24 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
-import { Route, Routes } from 'react-router'
-import AdminLayout from './components/AdminLayout'
-import ProtectedRoute from './components/ProtectedRoute'
-import Login from './Login'
-import Register from './Register'
-import Dashboard from './pages/Dashboard'
-import NotFound from './pages/NotFound'
-import { SettingsPage, UsersPage } from './pages/Placeholder'
+import { Route, Routes } from "react-router";
+import AdminLayout from "./components/AdminLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Login from "./Login";
+import Register from "./Register";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+import { SettingsPage, UsersPage } from "./pages/Placeholder";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ProtectedRoute />}>
+      <Route
+        path="/"
+        element={<ProtectedRoute />}
+      >
         <Route element={<AdminLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="users" element={<UsersPage />} />
-          <Route path="settings" element={<SettingsPage />} />
+          <Route
+            index
+            element={<Dashboard />}
+          />
+          <Route
+            path="users"
+            element={<UsersPage />}
+          />
+          <Route
+            path="settings"
+            element={<SettingsPage />}
+          />
         </Route>
       </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="*" element={<NotFound />} />
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+      <Route
+        path="*"
+        element={<NotFound />}
+      />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
